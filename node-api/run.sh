c999511216bc8aab7a56e0f3286f7300cd976214
vuits24 <42807353+vuits24@users.noreply.github.com>
2022-01-13 18:43:19 +0700

    Update run.sh

--- node-api/run.sh
+++ node-api/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 set +u
 
-#export REFRESH_TOKEN=$(bashio::config 'refresh_token')
+export REFRESH_TOKEN=$(bashio::config 'config')
 #bashio::log.info "Refresh token configured as ${REFRESH_TOKEN}."
 
 bashio::log.info "Starting bridge service."
