f2e332ab7486f4f5ecafa5c2a63237e7ba055e18
vuits24 <42807353+vuits24@users.noreply.github.com>
2022-01-13 20:37:28 +0700

    Update index.js

--- node-api/index.js
+++ node-api/index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const options = require('/data/options.json');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Vidu')
@@ -9,6 +10,7 @@ app.post('/api', (req,res) => {
     res.json([200, 'xin chào']);
 })
 
+console.log(options);
 app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening at http://0.0.0.0:${port}`)
 })
