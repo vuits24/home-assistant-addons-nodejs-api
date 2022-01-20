0bf509a58dc19b011ea058ce4df1a00915303530
vuits24 <42807353+vuits24@users.noreply.github.com>
2022-01-20 12:28:55 +0700

    Update index.js

--- node-api/index.js
+++ node-api/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const options = require('/data/options.json');
+const fs = require('fs');
 const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Vidu')
-})
-app.post('/api', (req,res) => {
-    res.json([200, 'xin chào']);
-})
-
-console.log(options);
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Example app listening at http://0.0.0.0:${port}`)
+const port = 3002;
+//var {google:googleDriverConfig, config:configData, timeUpdate, hostToken} =require("/data/options.json");
+const axios = require('axios');
+var saveConfig = async () => {
+  try {
+    var dataConfig = fs.readFileSync('/data/options.json', {encoding: 'utf-8'});
+    var dataConfigOb = JSON.parse(dataConfig);
+   var response = await axios.post('http://localhost:3001/setup-config', dataConfigOb);
+   console.log(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+ 
+};
+app.listen(port, "0.0.0.0",  () => {
+ // saveConfig();
+  console.log(`Lắng nghe sự kiện dưới trang http://0.0.0.0:${port}`)
 })
